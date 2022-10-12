0e3cd4ad8563c8abf77e9f7583d130982781e397
Kylie Merz <78387839+kmerz5@users.noreply.github.com>
2022-10-12 13:51:14 -0500

    Update index.php

--- index.php
+++ index.php
@@ -18,10 +18,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <<a class="nav-item nav-link" href="index.php">Home</a>
+          <a class="nav-item nav-link" href="index.php">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-item nav-link" href="customer.php">Customer</a>
