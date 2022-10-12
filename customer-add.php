1890207f073fb5d33f1292c41888c2558b6da941
Kylie Merz <78387839+kmerz5@users.noreply.github.com>
2022-10-12 14:01:52 -0500

    Create customer-add.php

---
+++ customer-add.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Add Instructor</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Add Customer</h1>
+<form method="post" action="customer-add-save.php">
+  <div class="mb-3">
+    <label for="CustomerFirstName" class="form-label">Name</label>
+    <input type="text" class="form-control" id="CustomerFirstName" aria-describedby="nameHelp" name="cName">
+    <div id="nameHelp" class="form-text">Enter the instructor's name.</div>
+  </div>
+  <div class="mb-3">
+    <label for="CustomerLastName" class="form-label">Name</label>
+    <input type="text" class="form-control" id="CustomerLastName" aria-describedby="nameHelp" name="cName">
+    <div id="nameHelp" class="form-text">Enter the instructor's name.</div>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
