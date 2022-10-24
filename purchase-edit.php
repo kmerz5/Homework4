01e350e520a06e4ffbdd5b61d3feef9196c69511
Kylie Merz <78387839+kmerz5@users.noreply.github.com>
2022-10-24 13:43:47 -0500

    Update purchase-edit.php

--- purchase-edit.php
+++ purchase-edit.php
@@ -1,1 +1,54 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Edit Purchase</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Edit Purchase</h1>
+<?php
+$servername = "localhost";
+$username = "kyliemer_Homework3";
+$password = "ChunkyMonkey01!";
+$dbname = "kyliemer_Homework3";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM Purchase WHERE Purchase_ID=?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $_POST['pid']);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+
+<form method="post" action="purchase-edit-save.php">
+  <div class="mb-3">
+    <label for="Customer_ID" class="form-label">ID</label>
+    <input type="int" class="form-control" id="Customer_ID" aria-describedby="nameHelp" name="cID">
+    <div id="nameHelp" class="form-text">Enter the customer's ID.</div>
+  </div>
+  <input type="hidden" name="pid" value="<?=$row['Purchase_ID']?>">
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
