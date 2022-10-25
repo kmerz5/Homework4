0e05826fb9028dac7c9e3550780341a71deeda1c
Kylie Merz <78387839+kmerz5@users.noreply.github.com>
2022-10-25 14:22:59 -0500

    Update purchase.php

--- purchase.php
+++ purchase.php
@@ -46,6 +46,32 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  switch ($_POST['saveType']) {
+    case 'Add':
+      $sqlAdd = "INSERT INTO Purchase (Customer_ID) values (?)";
+      $stmtAdd = $conn->prepare($sqlAdd);
+      $stmtAdd->bind_param("s", $_POST['cID']);
+      $stmtAdd->execute();
+      echo '<div class="alert alert-success" role="alert">New customer ID added.</div>';
+      break;
+    case 'Edit':
+      $sqlEdit = "UPDATE Purchase SET Customer_ID=? WHERE Purchase_ID=?";
+      $stmtEdit = $conn->prepare($sqlEdit);
+      $stmtEdit->bind_param("ii", $_POST['cID'],$_POST['pid']);
+      $stmtEdit->execute();
+      echo '<div class="alert alert-success" role="alert">Customer ID edited.</div>';
+      break;
+    case 'Delete':
+      $sqlDelete = "DELETE FROM Purchase WHERE Purchase_ID=?";
+      $stmtDelete = $conn->prepare($sqlDelete);
+      $stmtDelete->bind_param("i", $_POST['pid']);
+      $stmtDelete->execute();
+      echo '<div class="alert alert-success" role="alert">Customer deleted.</div>';
+      break;
+  }
+}
+    
 $sql = "SELECT * FROM Purchase";
 $result = $conn->query($sql);
 
@@ -56,18 +82,42 @@ if ($result->num_rows > 0) {
   <tr>
     <td><?=$row["Purchase_ID"]?></td>
     <td><?=$row["Customer_ID"]?></td>
-  </tr>
-     <td>
-       <form method="post" action="purchase-delete-save.php">
+   <td>
+       <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#editPurchase<?=$row["Purchase_ID"]?>">
+                Edit
+              </button>
+              <div class="modal fade" id="editPurchase<?=$row["Purchase_ID"]?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editPurchase<?=$row["Purchase_ID"]?>Label" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="editPurchase<?=$row["Purchase_ID"]?>Label">Edit Purchase</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      <form method="post" action="">
+                        <div class="mb-3">
+                          <label for="editPurchase<?=$row["Purchase_ID"]?>Name" class="form-label">Name</label>
+                          <input type="text" class="form-control" id="editPurchase<?=$row["Purchase_ID"]?>Name" aria-describedby="editPurchase<?=$row["Purchase_ID"]?>Help" name="cID" value="<?=$row['Customer_ID']?>">
+                          <div id="editPurchase<?=$row["Purchase_ID"]?>Help" class="form-text">Enter the customer's ID.</div>
+                        </div>
+                        <input type="hidden" name="pid" value="<?=$row['Purchase_ID']?>">
+                        <input type="hidden" name="saveType" value="Edit">
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </td>
+             <td>
+              <form method="post" action="">
                 <input type="hidden" name="pid" value="<?=$row["Purchase_ID"]?>" />
-                <input type="submit" value="Delete" class="btn btn-primary"/>
-                
+                <input type="hidden" name="saveType" value="Delete">
+                <input type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?')" value="Delete">
               </form>
-       <form method="post" action="purchase-edit.php">
-        <input type="hidden" name="pid" value="<?=$row["Purchase_ID"]?>" />
-        <input type="submit" value="Edit" class="btn btn-danger"/>
-      </form>
-    </td>
+            </td>
+          </tr>
+    
 <?php
   }
 } else {
@@ -75,7 +125,37 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
-  </tbody>
+   </tbody>
     </table>
-    <a href="purchase-add.php" class="btn btn-primary"> Add New<a/>
-      <a href="purchase-delete-save.php" class="btn btn-primary"> Delete Record<a/>
+    <br />
+     <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addPurchase">
+        Add New
+      </button>
+
+      <!-- Modal -->
+      <div class="modal fade" id="addPurchase" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addPurchaseLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="addPurchaseLabel">Add Purchase</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form method="post" action="">
+                <div class="mb-3">
+                  <label for="Customer_ID" class="form-label">Customer ID</label>
+                  <input type="text" class="form-control" id="Customer_ID" aria-describedby="nameHelp" name="cID">
+                  <div id="nameHelp" class="form-text">Enter the customer's ID.</div>
+                </div>
+                <input type="hidden" name="saveType" value="Add">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
