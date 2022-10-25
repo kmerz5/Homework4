d98b8a697ec86d6ad4f48ea2521f8cf1b51a3d60
Kylie Merz <78387839+kmerz5@users.noreply.github.com>
2022-10-25 12:28:10 -0500

    Update customer.php

--- customer.php
+++ customer.php
@@ -3,30 +3,13 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Customers</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <div class="container"
-         
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link" href="index.php">Home</a>
-      <a class="nav-item nav-link" href="customer.php">Customer</a>
-      <a class="nav-item nav-link" href="employee.php">Employee</a>
-      <a class="nav-item nav-link" href="purchase.php">Purchase</a>
-      <a class="nav-item nav-link" href="restaurant.php">Restaurant</a>
+    <div class="container">
       
-      
-    </div>
-  </div>
-</nav>
-
+<?php>
 $servername = "localhost";
 $username = "kyliemer_Homework3";
 $password = "ChunkyMonkey01!";
@@ -63,8 +46,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       break;
   }
 }
-
 ?>
+      
   <h1>Customers</h1>
   <table class="table table-striped">
   <thead>
@@ -72,12 +55,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       <th>Customer ID</th>
       <th>First Name</th>
       <th>Last Name</th>
-      
     </tr>
   </thead>
   <tbody>
-    <?php
     
+<?php
 $sql = "SELECT DISTINCT Customer_ID, Customer_FirstName, Customer_LastName FROM Customer";
 $result = $conn->query($sql);
 
@@ -88,8 +70,6 @@ if ($result->num_rows > 0) {
     
   <tr>
     <td><?=$row["Customer_ID"]?></td>
-    <td><?=$row["Customer_FirstName"]?></td>
-    <td><?=$row["Customer_LastName"]?></td>
     <td>
               <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editCustomer<?=$row["Customer_ID"]?>">
                 Edit
@@ -123,14 +103,14 @@ if ($result->num_rows > 0) {
               </div>
             </td>
     <td>
-       <form method="post" action="customer-delete-save.php">
+       <form method="post" action="">
                 <input type="hidden" name="cid" value="<?=$row["Customer_ID"]?>" />
                 <input type="hidden" name="saveType" value="Delete"
                 <input type="submit" value="Delete" class="btn btn-primary"/>
-                
               </form>
     </td>
   </tr>
+    
 <?php
   }
 } else {
